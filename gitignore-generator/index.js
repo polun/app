38e4a910761c83fb3d0b69f95999fb3cf1dce49d
zhenqiang <zhangzq@zigzen.cn>
2017-04-13 15:20:33 +0800

    update
    to be published to npm

--- gitignore-generator/index.js
+++ gitignore-generator/index.js
@@ -1,27 +1,3 @@
-const REPO_URL = 'https://github.com/github/gitignore.git';
-const path = require('path');
-const simpleGit = require('simple-git')(__dirname);
-const fs = require('fs');
-const argv = require('yargs').argv;
+const lib = require('./lib');
 
-const dataPath = path.join(__dirname, 'data');
-
-function getData(callback) {
-    if (fs.existsSync(dataPath)) {
-        const dataFileList = fs.readdirSync(dataPath);
-        const dict = {};
-        dataFileList.forEach((d) => {
-            if (path.extname(d) === '.gitignore') {
-                dict[d.slice(0, -10)] = path.join(dataPath, d);
-            }
-        });
-
-        callback(null, dict);
-    } else {
-        simpleGit.clone(REPO_URL, dataPath, (err) => {
-            if (err) throw err;
-        });
-    }
-}
-
-getData();
+lib.generate();
